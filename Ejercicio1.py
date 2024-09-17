a0b768b9687cd07fd1ad18fe0e84abe752a817a8
Jhon-Taipe <jhonvtaipe@gmail.com>
2024-09-16 21:28:12 -0500

    Ejer 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,3 +2,11 @@
 # variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña 
 # introducida por el usuario coincide con la guardada en la variable sin tener en cuenta
 # mayúsculas y minúsculas. (10 p)
+
+contrasena="Contraseña12345678" 
+contrasena1 = input("Por favor, ingresa la contraseña: ")
+if contrasena.lower()==contrasena1.lower():
+  print("Contraseña correcta")
+else:
+  print("Contraseña incorrecta")   
+
