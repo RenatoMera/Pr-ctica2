7ad94be8537747a761ecbf07709d18a9a16a5b8c
RenatoMera <zaiddrenato@gmail.com>
2024-09-16 20:29:12 -0500

    Practica2

---
+++ Ejercicio3.py
@@ -0,0 +1,2 @@
+# Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
+# Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla el tipo impositivo que le corresponde. (10p)
