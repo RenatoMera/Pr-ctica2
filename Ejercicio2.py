3de689ca5a1f224b1b022bea1bb6a62916aa59bb
Griselda-CH <gris01cordova@gmailcom>
2024-09-18 05:30:05 -0600

    Entregable

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,8 +1,22 @@
-# Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales.
-# Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no. (10p)
-Edad = int(input("Introduzca su Edad: "))
-Ingresos = float(input("Introduzca el salario que percibe: "))
-if Edad > 16 and Ingresos >=1000:
-    print("debes pagar impuestos")
+# Para tributar un determinado impuesto se debe ser mayor
+# de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales.
+# Escribir un programa que pregunte
+# al usuario su edad y sus ingresos mensuales y muestre por
+# pantalla si el usuario tiene que tributar o no. (10p)
+
+#Se soliciata el usuario su edad y se almacena en la variable Edad
+Edad= int(input("Ingrese su edad: "))
+ #Se solicita al usuario sus ingresos y se almacena en la variable ingresos
+ingresos= float(input("Ingrese su salario: "))
+ #Utilizamos if para verificar la edad y el salario del usuario 
+if Edad >16 and ingresos >= 100:
+ #Si se cumple la condicion imprime el resultado positivo
+   print(" Usted puede tributar. ")
+elif Edad < 16 and ingresos < 100:
+ #Si no se cumple la condicion imprime el resultado negativo
+   print(" Usted no puede tributar. ")
 else:
-    print("No debes pagar impuestos")    
+  #No cumple ninguna condicion
+  print(" Usted definitivamente no tributa. ")
+
+
--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,14 +1,32 @@
 # Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
-# Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla el tipo impositivo que le corresponde. (10p)
-Renta = float(input("Cual es tu renta anual?: "))
-if Renta <= 10000:
-    TipoIm = 5
-elif Renta <= 20000:
-    TipoIm = 15
-elif Renta <= 35000:
-    TipoIm = 20
-elif Renta <= 60000:
-    TipoIm = 30
-else:
-    TipoIm = 45
-print("Tienes que pagar: ", Renta * TipoIm / 100,)
+#    Renta	   	         Tipo impositivo
+#Menos de 10000€	         5%
+#Entre 10000€ y 20000€		15%
+#Entre 20000€ y 35000€	    20%
+#Entre 35000€ y 60000€	    30%
+#Más de 60000€	            45%
+# Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla 
+# el tipo impositivo que le corresponde. (10p)
+
+#Se soliciata al usuario que ingrese la renta mensual y se almacena en la variable renta
+renta=float(input("Ingrese la renta mensual: "))
+# Condicional para determinar el tipo impositivo dependiendo de la renta
+if renta <= 10000:
+    x = 5
+    print("el porcentaje es ", x)
+elif renta <= 20000:
+    x= 15
+    print("el porcentaje es ", x)
+elif renta <= 35000:
+    x=20
+    print("el porcentaje es ", x)
+elif renta <= 60000:
+    x=30
+    print("el porcentaje es ", x)
+elif renta > 60000:
+    x=45
+    print("el porcentaje es ", x)
+
+
+
+
