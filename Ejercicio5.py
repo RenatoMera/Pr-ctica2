d3e084604a5977ce2dd6887dce4ff1d7c2e4447f
Griselda-CH <gris01cordova@gmailcom>
2024-09-21 15:49:04 -0600

    Realizar Eje5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,70 +1,76 @@
 # En este ejercicio, tendrán total libertad para elegir la problemática y la mejor solución. 
 # Cada grupo podrá decidir su propio enfoque y desarrollar el software según sus criterios.
-# El grupo que presente la mejor solución será el único en recibir los 60 puntos asignados a esta pregunta.
-# Esto es un diccionario que almacena datos teniendo el alimento y las calorias
-alimentos_calorias = {
-    "manzana": 40,
-    "platano": 51,
-    "pollo": 190,
-    "arroz": 100,
-    "ensalada": 15,
-    "pan": 210,
-    "huevo": 75
-}
-#esta funcion muestra el menu, no tiene valores solo imprime el menu
-def mostrar_menu():
-    print("---- Calculadora de Calorías ----")
-    print("1. Ingresar alimento")
-    print("2. Mostrar total de calorías")
-    print("3. Recomendaciones")
-    print("4. Salir")
-# esta funcion permite ingresar el alimento en cuestion y sus calorias. Calcula las calorias y las sumas totales
-def ingresar_alimento(calorias_totales):
-    alimento = input("Ingrese el nombre del alimento: ").lower()
-    cantidad = int(input("Ingrese la cantidad (en porciones): "))
-    
-    if alimento in alimentos_calorias:
-        calorias = alimentos_calorias[alimento] * cantidad
-        calorias_totales += calorias
-        print(f"{alimento.title()} agregado. Calorías: {calorias}")
-    else:
-        print("Alimento no encontrado. Por favor, intente con otro.")
+# El grupo que presente la mejor solución será el único en recibir los 60 puntos asignados a 
+# esta pregunta.
+#Punto de Venta con 3 Productos 
 
-    return calorias_totales
-
-# esta funcion simplemente imprime el total de calorías consumidas hasta ese momento
-def mostrar_total(calorias_totales):
-    print(f"Total de calorías consumidas: {calorias_totales}")
-    
-# esta función ofrece recomendaciones al usuario en función de las calorías consumidas
-def recomendaciones(calorias_totales):
-    if calorias_totales < 1000:
-        print("Recomendación: ¡Aumenta tu ingesta calórica!")
-    elif calorias_totales > 1999:
-        print("Recomendación: ¡Considera reducir tu ingesta calórica!")
-    else:
-        print("¡Estás en el rango saludable de calorías!")
-        
-# esta funcion main() es la principal, Maneja el flujo de la ejecucion y permite al usuario elejir opciones del menu.
-# Hace que entre en un bucle si el usuario ingresa un codigo que no esta en lo relacionado ejemplo (opcion 10 no existe vuelves al menu principal)
+sabritacant=20
+sabritacosto=30
+galletacant=5
+galletacosto=19
+refrescant=10
+refrescosto=12
+#Defino main para utlizar el reorno si no se 
+# cumple alguna condicion
+# main:cuerpo de trabajo   
 def main():
-    calorias_totales = 0
     
-    while True:
-        mostrar_menu()
-        opcion = input("Seleccione una opción: ")
+    print("**********VENTAS**********")
+    sabritas=int(input("¿Cuantas sabritas venderas?"))
+    galletas=int(input("¿Cuantas galletas venderas?"))
+    refrescos=int(input("¿Cuantas refrescos venderas?"))
+    #While; Mientras las condiciones se cumplan, imprimer las cantidades disponible 
+    #al usuario y que ingrese cantidades validas y el while lo retorna la main  principal 
+    # 
+    while (sabritas>sabritacant)or(galletas>galletacant)or(refrescos>refrescant): 
+        
+        print("solo dispogo de",sabritacant,"sabritas")
+        print("solo dispogo de",galletacant,"galletas")
+        print("solo dispogo de",refrescant,"refrescos")
+        print("ingresar una cantidad disponible")
+        
+        
+        return main()
 
-        if opcion == "1":
-            calorias_totales = ingresar_alimento(calorias_totales)
-        elif opcion == "2":
-            mostrar_total(calorias_totales)
-        elif opcion == "3":
-            recomendaciones(calorias_totales)
-        elif opcion == "4":
-            print("Saliendo de la aplicación. ¡Hasta luego!")
-            break
-        else:
-            print("Opción no válida. Intente nuevamente.")
-# este bloque garantiza que la función main() se ejecute
-if __name__ == "__main__":
-    main()
+    
+    # Cuando el if se cumple  y tienes las cnatidades diponibles se reliza las 
+    #operaciones
+    if sabritas<=sabritacant:
+    #Calculo el valor en pesos de los productos 
+       ventasabritas=sabritas*sabritacosto
+       ventagalletas=galletas*galletacosto
+       ventarefrescos=refrescos*refrescosto
+    #Sumo el total de cantidades de cada producto 
+       costoproduc = ventasabritas + ventagalletas + ventarefrescos 
+    #Imprimo el total de la venta 
+       print("la venta total es:",costoproduc)
+    #Pregunto al usuario con cuanto dinero pagara
+       pagarcliente=int(input("¿con cuanto dinero pagara el cliente?"))
+    #Realizo una resta para saber el cambio que se le da la cliente 
+       cambio=pagarcliente-costoproduc
+    #Imprimo cambio que se dara al cliente
+       print("tendrias que devolver al cliente de cambio:",cambio)
+         
+    elif galletas<=galletacant:
+        
+        ventasabritas=sabritas*sabritacosto
+        ventagalletas=galletas*galletacosto
+        ventarefrescos=refrescos*refrescosto
+        costoproduc = ventasabritas + ventagalletas + ventarefrescos 
+        print("la venta total es:",costoproduc)
+        pagarcliente=int(input("¿con cuanto dinero pagara el cliente?"))
+        cambio=pagarcliente-costoproduc
+        print("tendrias que devolver al cliente de cambio:",cambio)  
+        
+    elif refrescos<=refrescant:
+    
+        ventasabritas=sabritas*sabritacosto
+        ventagalletas=galletas*galletacosto
+        ventarefrescos=refrescos*refrescosto
+        costoproduc = ventasabritas + ventagalletas + ventarefrescos 
+        print("la venta total es:",costoproduc)
+        pagarcliente=int(input("¿con cuanto dinero pagara el cliente?"))
+        cambio=pagarcliente-costoproduc
+        print("tendrias que devolver al cliente de cambio:",cambio)
+        
+main()           
