200e8cef4e0ba45a52781a7fb6f48436cfee5358
Griselda-CH <gris01cordova@gmailcom>
2024-09-21 15:53:11 -0600

    Realizar eje5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,6 +1,8 @@
 # En este ejercicio, tendrán total libertad para elegir la problemática y la mejor solución. 
 # Cada grupo podrá decidir su propio enfoque y desarrollar el software según sus criterios.
-# El grupo que presente la mejor solución será el único en recibir los 60 puntos asignados a esta pregunta.
+# El grupo que presente la mejor solución será el único en recibir los 60
+# puntos asignados a esta pregunta.
+#Punto de Venta
 
 sabritacant=20
 sabritacosto=30
